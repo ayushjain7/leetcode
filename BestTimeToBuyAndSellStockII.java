9f15851318e76177498d2ff9cadf063ed98eaed1
Ayush Jain <ayushjain7@gmail.com>
2016-03-10 13:40:13 -0500

    Create BestTimeToBuyAndSellStockII.java

---
+++ BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0, i=0;
+        boolean bought = false;
+        if(prices == null){
+            return 0;
+        }
+        for(; i<prices.length-1; i++){
+            if(bought){
+                if(prices[i] > prices[i+1]){
+                    bought = false;
+                    maxProfit += prices[i];
+                }
+            }
+            else{
+                if(prices[i] < prices[i+1]){
+                    bought = true;
+                    maxProfit -= prices[i];
+                }
+            }
+        }
+        if(bought){
+            bought = false;
+            maxProfit += prices[i];
+        }
+        return maxProfit;
+    }
+}
